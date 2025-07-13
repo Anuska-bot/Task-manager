336a5cc6d2125382c65c21b9350697252c8079b8
ANUSKA MRIDHA <anuskaringa@gmail.com>
2025-07-13 21:06:41 +0530

    Fixed HTML & removed merge conflicts

--- script.js
+++ script.js
@@ -22,50 +22,6 @@ listContainer.addEventListener("click", function (e) {
         if(e.target.tagName === "LI") {
             e.target.classList.toggle("checked");
             savedata();
-        }
-        else if(e.target.tagName === "SPAN"){
-            e.target.parentElement.remove();
-            savedata();
-        }
-}, false);
-
-function savedata(){
-    localStorage.setItem("Data", listContainer.innerHTML);
-}
-
-function showTask(){
-    listContainer.innerHTML=localStorage.getItem('Data');   
-}
-showTask();
-inputBox.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-        addTask();
-    }
-});
-const inputBox= document.getElementById("input-box");
-const listContainer=document.getElementById("list-contain");
-
-function addTask(){
-    if(inputBox.value ===''){
-        alert("You must Write something!");
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value="";
-    savedata();
-}
-
-
-listContainer.addEventListener("click", function (e) {
-        if(e.target.tagName === "LI") {
-            e.target.classList.toggle("checked");
-            savedata();
             updateCounter(); 
         }
         else if(e.target.tagName === "SPAN"){
