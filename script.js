96bdef4e20b85e28a9a5fb1903e7cf1ac8f96b8c
ANUSKA MRIDHA <anuskaringa@gmail.com>
2025-07-13 20:43:14 +0530

    initial commit

--- script.js
+++ script.js
@@ -42,4 +42,72 @@ inputBox.addEventListener("keydown", function (e) {
         addTask();
     }
 });
+const inputBox= document.getElementById("input-box");
+const listContainer=document.getElementById("list-contain");
+
+function addTask(){
+    if(inputBox.value ===''){
+        alert("You must Write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    savedata();
+}
+
+
+listContainer.addEventListener("click", function (e) {
+        if(e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            savedata();
+            updateCounter(); 
+        }
+        else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            savedata();
+            updateCounter(); 
+        }
+}, false);
+
+function savedata(){
+    localStorage.setItem("Data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem('Data');   
+}
+showTask();
+updateCounter(); 
+
+inputBox.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTask();
+        updateCounter();
+    }
+});
+function clearAll() {
+    listContainer.innerHTML = "";
+    savedata();
+    updateCounter();
+}
+let timestamp = new Date().toLocaleString();
+li.innerHTML = `${inputBox.value} <span class="timestamp">${timestamp}</span>`;
+
+li.addEventListener("dblclick", function () {
+    let editedText = prompt("Edit your task:", li.firstChild.textContent.trim());
+    if (editedText) {
+        li.firstChild.textContent = editedText + " ";
+        savedata();
+    }
+});
+function updateCounter() {
+    const tasks = listContainer.getElementsByTagName("li");
+    document.getElementById("counter").innerText = `Total Tasks: ${tasks.length}`;
+}
 
